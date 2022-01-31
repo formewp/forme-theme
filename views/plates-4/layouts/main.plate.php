3e1d70b45302390d16b4536b3530350bbd2b4f0b
Moussa Clarke <moussaclarke@gmail.com>
2022-01-31 08:14:32 +0000

    chore: remove useless tag

--- views/plates-4/layouts/main.plate.php
+++ views/plates-4/layouts/main.plate.php
@@ -5,7 +5,6 @@
     <title>Forme</title>
     <meta charset="<?php bloginfo('charset');?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="profile" href="http://gmpg.org/xfn/11">
     <meta name="description" content="">
     <meta name="author" content="">
     <?php wp_head();?>
