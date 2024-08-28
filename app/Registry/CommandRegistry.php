01e94fd309751c42fec5398a2609850b28346c87
Moussa Clarke <moussaclarke@gmail.com>
2024-08-28 15:16:56 +0100

    feat: command registry

---
+++ app/Registry/CommandRegistry.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VendorName\ReplaceMeTheme\Registry;
+
+use Forme\Framework\Registry\RegistryInterface;
+
+final class CommandRegistry implements RegistryInterface
+{
+    public function register(): void
+    {
+        if (! PHP_SAPI === 'cli') {
+            return;
+        }
+        $classFiles = glob(__DIR__ . '/../FormeCommand/*');
+        foreach ($classFiles as $classFile) {
+            require_once $classFile;
+        }
+    }
+}
